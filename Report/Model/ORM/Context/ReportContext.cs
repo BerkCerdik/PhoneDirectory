8c9e07032587db24a133556970c418c92ff0208e
BerkCerdik <berkcerdik@gmail.com>
2021-02-13 16:06:31 +0300

    DB Connection Completed

--- Report/Model/ORM/Context/ReportContext.cs
+++ Report/Model/ORM/Context/ReportContext.cs
@@ -9,8 +9,11 @@ using Report.Model.ORM.Entities;
 namespace Report.Model.ORM.Context
 {
     public class ReportContext :DbContext
-    { 
-        // DB Connection
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(@"Server=ec2-52-209-134-160.eu-west-1.compute.amazonaws.com;Database=d68fhj4cdnhoh0;UID=ciinwhxmoazmfy;PWD=ba797a428b37492d5666c8e870cd667995b0e200ba29cd4bd85ac98f3bdf75cb;SSL Mode= Require;TrustServerCertificate=True");
+        }
 
         public DbSet<ReportEntity> Reports{ get; set; }
 
--- Report/Startup.cs
+++ Report/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Report.Model.ORM.Context;
 
 namespace Report
 {
@@ -26,6 +27,7 @@ namespace Report
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddDbContext<ReportContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
